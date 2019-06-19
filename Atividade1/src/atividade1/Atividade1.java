7794d916f7e5a4f5651986ebc358ec0a2115199f
Andrey <andrey.henrique28@gmail.com>
2019-06-19 10:38:46 -0300

    Atividade 1

---
+++ Atividade1/src/atividade1/Atividade1.java
@@ -0,0 +1,14 @@
+package atividade1;
+public class Atividade1 {
+    public static void main(String[] args) {
+        int x = 43;
+        if(x < 15) {
+            System.out.println("X é menor que 15");
+        } else if(x > 100) {
+            System.out.println("X é maior que 100");
+        } else {
+            System.out.println("X está entre 15 e 100");
+        }
+    }
+    
+}
